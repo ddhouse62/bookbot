a05d9b6a0513bf432bb97302ba6b12f4a665689c
ddhouse62 <house.dalton62@gmail.com>
2024-12-05 23:30:48 -0600

    Adds ability to count words in a book

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        counted_words = word_count(file_contents)
+        print(counted_words)
+
+def word_count(book_to_count):
+    book_words = book_to_count.split()
+    num_book_words = len(book_words)
+    return num_book_words
 
 main()

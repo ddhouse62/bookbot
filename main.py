d99a8d7f9ce5d1286d54d9d7a26f77981f6175ad
ddhouse62 <house.dalton62@gmail.com>
2024-12-05 23:34:12 -0600

    Refactor for easier decoupling

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        counted_words = word_count(file_contents)
-        print(counted_words)
+    path_to_book = "books/frankenstein.txt"
+    text = get_book_text(path_to_book)
+    counted_words = word_count(text)
+    print(counted_words)
 
 def word_count(book_to_count):
-    book_words = book_to_count.split()
-    num_book_words = len(book_words)
-    return num_book_words
+    return len(book_to_count.split())
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
